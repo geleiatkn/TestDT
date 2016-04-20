d544436407d9669a19a2f7c2180247ccba35f3f8
Thiago Campos <Thiago.Campos@dynatrace.com>
2016-04-20 16:15:06 -0300

    Adicionando nova versão

---
+++ src/br/com/servlet/teste/Conta.java
@@ -0,0 +1,20 @@
+package br.com.servlet.teste;
+public class Conta {
+  int numero;
+  String dono;
+  double saldo;
+  double limite;
+     
+  boolean saca(double quantidade) {
+	     double novoSaldo = this.saldo - quantidade; 
+	     this.saldo = novoSaldo;
+	     return true;
+  }
+  
+  void deposita(double quantidade) {
+	     this.saldo += quantidade;
+	   }
+  
+  
+}
+
---
+++ src/br/com/servlet/teste/Programa.java
@@ -0,0 +1,14 @@
+package br.com.servlet.teste;
+public class Programa {
+	  public static void main(String[] args) {
+	    Conta minhaConta;
+	    minhaConta = new Conta();
+
+	    minhaConta.dono = "Duke";
+	    minhaConta.saldo = 1000.0;
+
+	    System.out.println("Saldo atual: " + minhaConta.saldo);
+	  }
+
+	}
+
